66a6dd9f42b148fadd6587861a884262327f5451
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-11-22 20:42:00 +0900

    🔧 useAmountInput 훅의 초기값 설정 방식 개선

--- src/hooks/useAmountInput.ts
+++ src/hooks/useAmountInput.ts
@@ -1,6 +1,6 @@
 import { ChangeEvent, useState } from 'react';
 
-export const useAmountInput = (initialValue: number = 0) => {
+export const useAmountInput = (initialValue = 0) => {
   const [amount, setAmount] = useState(initialValue);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
